38e92f2ee888b463664381e734064c95d6b081ae
Joshua Davian <joshuadavian@gmail.com>
2020-10-01 07:17:49 +0700

    welcome back activity

--- app/src/main/java/polinema/ac/id/dtschapter03_starter/fragment/WelcomeFirstFragment.java
+++ app/src/main/java/polinema/ac/id/dtschapter03_starter/fragment/WelcomeFirstFragment.java
@@ -1,5 +1,6 @@
 package polinema.ac.id.dtschapter03_starter.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,24 +11,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import polinema.ac.id.dtschapter03_starter.R;
+import polinema.ac.id.dtschapter03_starter.WelcomeBackActivity;
 
 public class WelcomeFirstFragment extends Fragment {
     View view;
     Button btnToSecondFragment;
+    TextView tvLogin;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.welcome_first, container, false);
         btnToSecondFragment = (Button) view.findViewById(R.id.btn_get_started_1);
+        tvLogin = (TextView) view.findViewById(R.id.login_text_1);
+
         btnToSecondFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadFragment(new WelcomeSecondFragment());
             }
         });
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), WelcomeBackActivity.class);
+                startActivity(i);
+            }
+        });
+
         return view;
     }
 
--- app/src/main/java/polinema/ac/id/dtschapter03_starter/fragment/WelcomeSecondFragment.java
+++ app/src/main/java/polinema/ac/id/dtschapter03_starter/fragment/WelcomeSecondFragment.java
@@ -1,5 +1,6 @@
 package polinema.ac.id.dtschapter03_starter.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,18 +11,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import polinema.ac.id.dtschapter03_starter.R;
+import polinema.ac.id.dtschapter03_starter.WelcomeBackActivity;
 
 public class WelcomeSecondFragment extends Fragment {
     View view;
     Button btnToThirdFragment;
+    TextView tvLogin;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.welcome_second, container, false);
         btnToThirdFragment = (Button) view.findViewById(R.id.btn_get_started_2);
+        tvLogin = (TextView) view.findViewById(R.id.login_text_2);
 
         btnToThirdFragment.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +34,15 @@ public class WelcomeSecondFragment extends Fragment {
                 loadFragment(new WelcomeThirdFragment());
             }
         });
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), WelcomeBackActivity.class);
+                startActivity(i);
+            }
+        });
+
         return view;
     }
 
