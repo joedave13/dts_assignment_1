f78c5d19639e39afa832d961d3186d672f49283b
Joshua Davian <joshuadavian@gmail.com>
2020-10-01 07:20:27 +0700

    fixing welcome fragment third

--- app/src/main/java/polinema/ac/id/dtschapter03_starter/fragment/WelcomeThirdFragment.java
+++ app/src/main/java/polinema/ac/id/dtschapter03_starter/fragment/WelcomeThirdFragment.java
@@ -34,6 +34,7 @@ public class WelcomeThirdFragment extends Fragment {
                 loadFragment(new WelcomeFirstFragment());
             }
         });
+
         tvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
