ea9363e07d09db93ab29f9346b1394baa3f156ec
Joshua Davian <joshuadavian@gmail.com>
2020-10-01 12:22:55 +0700

    adding intent

--- app/src/main/java/polinema/ac/id/dtschapter03_starter/ForgotPasswordActivity.java
+++ app/src/main/java/polinema/ac/id/dtschapter03_starter/ForgotPasswordActivity.java
@@ -1,13 +1,27 @@
 package polinema.ac.id.dtschapter03_starter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+    Button btnForgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        btnForgot = (Button) findViewById(R.id.btn_forgot);
+
+        btnForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ResetPasswordActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/polinema/ac/id/dtschapter03_starter/WelcomeBackActivity.java
+++ app/src/main/java/polinema/ac/id/dtschapter03_starter/WelcomeBackActivity.java
@@ -1,13 +1,40 @@
 package polinema.ac.id.dtschapter03_starter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeBackActivity extends AppCompatActivity {
+    TextView tv_forgot;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_back);
+
+        tv_forgot = (TextView) findViewById(R.id.tv_forgot_password);
+        btnLogin = (Button) findViewById(R.id.btn_sign_in);
+
+        tv_forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ForgotPasswordActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SuccessActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
